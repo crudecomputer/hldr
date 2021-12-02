d5d388086e66a8607e792ba7b64eec2caf04c262
kevlarr <6306496+kevlarr@users.noreply.github.com>
2021-12-02 09:02:32 -0500

    set up files

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,75 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use clap::{Parser, crate_version};
 
-/// Easy PostgreSQL data seeding
+/// Placeholder: Easy PostgreSQL data seeding
 #[derive(Parser, Debug)]
 #[clap(version = crate_version!())]
-struct Opts {
-    /// Database connection string - for allowed formats see https://docs.rs/postgres/0.19.2/postgres/config/struct.Config.html
+struct Command {
+    /// Connection string - for allowed formats see: https://docs.rs/postgres/0.19.2/postgres/config/struct.Config.html)
     #[clap(short = 'd', long = "database-conn", name = "CONN")]
-    database_connstr: String,
+    database_conn: Option<String>,
 
     /// Path to the data file to load
-    #[clap(short = 'f', long = "file", name = "FILE")]
-    filepath: PathBuf,
+    #[clap(short = 'f', long = "data-file", name = "FILE")]
+    data_file: Option<PathBuf>,
 
-    /// Commits the transaction, which is rolled back by default
+    /// Commit the transaction, rolled back by default
     #[clap(long = "commit")]
     commit: bool
 }
 
+struct Vars {
+    database_conn: Option<String>,
+    data_file: Option<PathBuf>,
+}
+
+impl Vars {
+    fn empty() -> Self {
+        Self { database_conn: None, data_file: None }
+    }
+}
+
 fn main() {
-    let opts: Opts = Opts::parse();
-    hldr::seed(&opts.database_connstr, &opts.filepath, opts.commit);
+    let cmd = Command::parse();
+
+    match (cmd.database_conn, cmd.data_file) {
+        (Some(database_conn), Some(data_file)) => {
+            hldr::place(&database_conn, &data_file, cmd.commit);
+        }
+        (dc, df) => {
+            let vars = vars_from_file();
+            hldr::place(
+                &dc.unwrap_or_else(|| vars.database_conn.expect("database_conn not found in file")),
+                &df.or_else(|| vars.data_file).unwrap_or_else(|| PathBuf::from("place.hldr")),
+                cmd.commit,
+            )
+        }
+    }
+}
+
+fn vars_from_file() -> Vars {
+    let varfile = PathBuf::from(".placehldr");
+
+    if !varfile.exists() {
+        panic!(".placehldr file is missing");
+    }
+
+    if !varfile.is_file() {
+        panic!(".placehldr is not a file");
+    }
+
+    let mut vars = Vars::empty();
+
+    for item in dotenv::from_path_iter(&varfile).unwrap() {
+        let (key, val) = item.unwrap();
+
+        match key.as_ref() {
+            "database_conn" => vars.database_conn = Some(val),
+            "data_file" => vars.data_file = Some(PathBuf::from(&val)),
+            _ => panic!("Unexpected variable: {}", key),
+        }
+    }
+
+    vars
 }
