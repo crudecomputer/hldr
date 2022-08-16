08662ab265dd6c9a9ee4b663e58d23421bfc3732
kevlarr <6306496+kevlarr@users.noreply.github.com>
2022-08-15 20:49:44 -0400

    start setting up parser

--- src/v2/lex/mod.rs
+++ src/v2/lex/mod.rs
@@ -5,7 +5,7 @@ mod tokenizer;
 
 pub use error::{LexError, LexErrorKind};
 pub use position::Position;
-pub use tokenizer::{Keyword, Token, TokenPosition};
+pub use tokenizer::{Keyword, Number, Symbol, Token, TokenPosition, Whitespace};
 
 pub fn lex(text: &str) -> Result<Vec<TokenPosition>, LexError> {
     Ok(tokenizer::Tokenizer::new().tokenize(text)?.tokens)
