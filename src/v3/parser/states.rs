dc281c542e58d387b8876505e1c2616f1f0d52d4
kevlarr <6306496+kevlarr@users.noreply.github.com>
2024-03-22 10:46:37 -0400

    submodules for schema/table states

--- src/v3/parser/states.rs
+++ src/v3/parser/states.rs
@@ -99,181 +99,189 @@ impl State for Root {
                 to(Root)
             }
             Tkn::Keyword(Kwd::Schema) => {
-                to(DeclaringSchema)
+                to(schema_states::DeclaringSchema)
             }
             Tkn::Keyword(Kwd::Table) => {
-                to(DeclaringTable)
+                to(table_states::DeclaringTable)
             }
             _ => Err(ParseError),
         }
     }
 }
 
-/// State after receiving the `schema` keyword for declaration.
-struct DeclaringSchema;
+mod schema_states {
+    use super::*;
 
-impl State for DeclaringSchema {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        match t {
-            Tkn::Identifier(ident) | Tkn::QuotedIdentifier(ident) => {
-                to(ReceivedSchemaName(ident))
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
+    /// State after receiving the `schema` keyword for declaration.
+    pub struct DeclaringSchema;
 
-/// State after receiving the schema name during declaration.
-struct ReceivedSchemaName(String);
-
-impl State for ReceivedSchemaName {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        let schema_name = mem::take(&mut self.0);
-
-        match t {
-            Tkn::Keyword(Kwd::As) => {
-                to(DeclaringSchemaAlias(schema_name))
-            }
-            Tkn::Symbol(Sym::ParenLeft) => {
-                ctx.push_schema(schema_name, None);
-                to(InSchemaScope)
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
-
-/// State after receiving the `as` keyword during schema declaration.
-struct DeclaringSchemaAlias(String);
-
-impl State for DeclaringSchemaAlias {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        let schema_name = mem::take(&mut self.0);
-
-        match t {
-            // Unlike the true database name, aliases do not support quoted identifiers
-            Tkn::Identifier(ident) => {
-                to(ReceivedSchemaAlias(schema_name, ident))
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
-
-struct ReceivedSchemaAlias(String, String);
-
-impl State for ReceivedSchemaAlias {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        match t {
-            Tkn::Symbol(Sym::ParenLeft) => {
-                let schema_name = mem::take(&mut self.0);
-                let alias = mem::take(&mut self.1);
-                ctx.push_schema(schema_name, Some(alias));
-                to(InSchemaScope)
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
-
-/// State
-struct InSchemaScope;
-
-impl State for InSchemaScope {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        match t {
-            Tkn::Symbol(Sym::ParenRight) => {
-                let schema = ctx.pop_schema_or_panic();
-                ctx.push_schema_to_root_or_panic(schema);
-                to(Root)
-            }
-            Tkn::Keyword(Kwd::Table) => {
-                to(DeclaringTable)
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
-
-/// State after receiving the `table` keyword for declaration.
-pub struct DeclaringTable;
-
-impl State for DeclaringTable {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        match t {
-            Tkn::Identifier(ident) | Tkn::QuotedIdentifier(ident) => {
-                to(ReceivedTableName(ident))
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
-
-/// State after receiving the table name during declaration.
-struct ReceivedTableName(String);
-
-impl State for ReceivedTableName {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        let table_name = mem::take(&mut self.0);
-
-        match t {
-            Tkn::Keyword(Kwd::As) => {
-                to(DeclaringTableAlias(table_name))
-            }
-            Tkn::Symbol(Sym::ParenLeft) => {
-                ctx.push_table(table_name, None);
-                to(InTableScope)
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
-
-struct DeclaringTableAlias(String);
-
-impl State for DeclaringTableAlias {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        let table_name = mem::take(&mut self.0);
-
-        match t {
-            Tkn::Identifier(ident) => {
-                to(ReceivedTableAlias(table_name, ident))
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
-
-struct ReceivedTableAlias(String, String);
-
-impl State for ReceivedTableAlias {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        match t {
-            Tkn::Symbol(Sym::ParenLeft) => {
-                let table_name = mem::take(&mut self.0);
-                let alias = mem::take(&mut self.1);
-                ctx.push_table(table_name, Some(alias));
-                to(InTableScope)
-            }
-            _ => Err(ParseError),
-        }
-    }
-}
-
-struct InTableScope;
-
-impl State for InTableScope {
-    fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
-        match t {
-            Tkn::Symbol(Sym::ParenRight) => {
-                let table = ctx.pop_table_or_panic();
-
-                match ctx.push_table_to_parent_or_panic(table) {
-                    PushedTableTo::TreeRoot => to(Root),
-                    PushedTableTo::Schema => to(InSchemaScope),
+    impl State for DeclaringSchema {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            match t {
+                Tkn::Identifier(ident) | Tkn::QuotedIdentifier(ident) => {
+                    to(ReceivedSchemaName(ident))
                 }
+                _ => Err(ParseError),
             }
-            _ => Err(ParseError),
+        }
+    }
+
+    /// State after receiving the schema name during declaration.
+    struct ReceivedSchemaName(String);
+
+    impl State for ReceivedSchemaName {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            let schema_name = mem::take(&mut self.0);
+
+            match t {
+                Tkn::Keyword(Kwd::As) => {
+                    to(DeclaringSchemaAlias(schema_name))
+                }
+                Tkn::Symbol(Sym::ParenLeft) => {
+                    ctx.push_schema(schema_name, None);
+                    to(InSchemaScope)
+                }
+                _ => Err(ParseError),
+            }
+        }
+    }
+
+    /// State after receiving the `as` keyword during schema declaration.
+    struct DeclaringSchemaAlias(String);
+
+    impl State for DeclaringSchemaAlias {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            let schema_name = mem::take(&mut self.0);
+
+            match t {
+                // Unlike the true database name, aliases do not support quoted identifiers
+                Tkn::Identifier(ident) => {
+                    to(ReceivedSchemaAlias(schema_name, ident))
+                }
+                _ => Err(ParseError),
+            }
+        }
+    }
+
+    struct ReceivedSchemaAlias(String, String);
+
+    impl State for ReceivedSchemaAlias {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            match t {
+                Tkn::Symbol(Sym::ParenLeft) => {
+                    let schema_name = mem::take(&mut self.0);
+                    let alias = mem::take(&mut self.1);
+                    ctx.push_schema(schema_name, Some(alias));
+                    to(InSchemaScope)
+                }
+                _ => Err(ParseError),
+            }
+        }
+    }
+
+    /// State
+    pub struct InSchemaScope;
+
+    impl State for InSchemaScope {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            match t {
+                Tkn::Symbol(Sym::ParenRight) => {
+                    let schema = ctx.pop_schema_or_panic();
+                    ctx.push_schema_to_root_or_panic(schema);
+                    to(Root)
+                }
+                Tkn::Keyword(Kwd::Table) => {
+                    to(table_states::DeclaringTable)
+                }
+                _ => Err(ParseError),
+            }
+        }
+    }
+}
+
+mod table_states {
+    use super::*;
+
+    /// State after receiving the `table` keyword for declaration.
+    pub struct DeclaringTable;
+
+    impl State for DeclaringTable {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            match t {
+                Tkn::Identifier(ident) | Tkn::QuotedIdentifier(ident) => {
+                    to(ReceivedTableName(ident))
+                }
+                _ => Err(ParseError),
+            }
+        }
+    }
+
+    /// State after receiving the table name during declaration.
+    struct ReceivedTableName(String);
+
+    impl State for ReceivedTableName {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            let table_name = mem::take(&mut self.0);
+
+            match t {
+                Tkn::Keyword(Kwd::As) => {
+                    to(DeclaringTableAlias(table_name))
+                }
+                Tkn::Symbol(Sym::ParenLeft) => {
+                    ctx.push_table(table_name, None);
+                    to(InTableScope)
+                }
+                _ => Err(ParseError),
+            }
+        }
+    }
+
+    struct DeclaringTableAlias(String);
+
+    impl State for DeclaringTableAlias {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            let table_name = mem::take(&mut self.0);
+
+            match t {
+                Tkn::Identifier(ident) => {
+                    to(ReceivedTableAlias(table_name, ident))
+                }
+                _ => Err(ParseError),
+            }
+        }
+    }
+
+    struct ReceivedTableAlias(String, String);
+
+    impl State for ReceivedTableAlias {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            match t {
+                Tkn::Symbol(Sym::ParenLeft) => {
+                    let table_name = mem::take(&mut self.0);
+                    let alias = mem::take(&mut self.1);
+                    ctx.push_table(table_name, Some(alias));
+                    to(InTableScope)
+                }
+                _ => Err(ParseError),
+            }
+        }
+    }
+
+    pub struct InTableScope;
+
+    impl State for InTableScope {
+        fn receive(&mut self, ctx: &mut Context, t: Tkn) -> ParseResult {
+            match t {
+                Tkn::Symbol(Sym::ParenRight) => {
+                    let table = ctx.pop_table_or_panic();
+
+                    match ctx.push_table_to_parent_or_panic(table) {
+                        PushedTableTo::TreeRoot => to(Root),
+                        PushedTableTo::Schema => to(schema_states::InSchemaScope),
+                    }
+                }
+                _ => Err(ParseError),
+            }
         }
     }
 }
